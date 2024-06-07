eb161e6da287036a89f752e0a7e52ed0a93d3c7e
Flatr0n <flatron.2020@mail.ru>
2024-06-07 13:22:46 +0800

    11-learn

--- 01-learn/src/App.js
+++ 01-learn/src/App.js
@@ -3,21 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <header className='heder'>
+        <img src='https://image.pngaaa.com/828/105828-middle.png'/>
+        </header>
+        <nav className='nav'>
+          <div><a href='#a' >Profile</a></div>
+          <div><a href='#b' >Masseges</a></div>
+          <div><a href='#c'>News</a></div>
+          <div><a href='#d' >Music</a></div>
+          <div><a href='#s'>Settings</a></div>
+        </nav>
+        <div className='content'>
+          <div>
+            <img src='https://i.pinimg.com/originals/0f/ab/7b/0fab7b5b292820bcd560a44b744597a2.jpg'></img>   
+          </div>  
+          <div className='ava'>
+            <img src='https://image.pngaaa.com/828/105828-middle.png'></img>
+            Yuor profile
+          </div>
+          <div>new post</div>
+          <div>post</div>
+          <div>old post</div>
+          <div>post</div>
+        </div>
     </div>
   );
 }
